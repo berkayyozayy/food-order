701b0f7c150be7f6d8186f9e8898c505df3560a0
Berkay <berkay@Berkay-MacBook-Air.local>
2022-12-24 00:24:57 +0300

    add meals

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = (props) => {
+  return (
+    <section className={classes.summary}>
+      <h2>Everything for Meals</h2>
+      <p>Choose your favoritossss</p>
+      <p>Afiyetsss</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
