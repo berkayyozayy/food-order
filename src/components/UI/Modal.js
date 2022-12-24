cd65bd265467f5a160526aad943fae6600f48c43
Berkay <berkay@Berkay-MacBook-Air.local>
2022-12-24 13:36:37 +0300

    feat: close the modal when click backdrop

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -19,7 +19,10 @@ const portalOverlayEl = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalOverlayEl)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalOverlayEl
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalOverlayEl
